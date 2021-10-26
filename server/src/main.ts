6108a4e79e94fb42db1912357bf75eb331eb8501
utochkin <vlad.uto4kin@yandex.ru>
2021-10-26 18:41:32 +0400

    init back

---
+++ server/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.enableCors({
+    origin: ['http://localhost:8080'],
+    credentials: true,
+  });
+  await app.listen(3000);
+}
+bootstrap();
