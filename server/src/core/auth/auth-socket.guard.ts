1bf97fa572cbedee64645ed6c00aa89009703ec8
Andris Tsildermanis <yanich.2213@gmail.com>
2021-11-09 13:03:00 +0400

    add: - auth guard

---
+++ server/src/core/auth/auth-socket.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import AuthService from './auth.service';
+
+@Injectable()
+export default class WsGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const data = context.switchToWs().getData();
+    if (typeof data !== 'object' || !('accessToken' in data)) return false;
+
+    return await this.authService.verify(data.accessToken);
+  }
+}
