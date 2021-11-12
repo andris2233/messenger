870bc2c5c54f304ef81d2adf0604567ae52b6c04
Andris Tsildermanis <yanich.2213@gmail.com>
2021-11-12 20:43:11 +0400

    fix: - move host into config

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -8,3 +8,5 @@ const NODE_ENV: string = process.env.NODE_ENV;
 export const databaseConnectionOptions: SequelizeModuleOptions = NODE_ENV === 'prod' ? prod.dbOptions : dev.dbOptions;
 
 export const JWT_SECRET_KEY: string = NODE_ENV === 'prod' ? prod.JWT_SECRET_KEY : dev.JWT_SECRET_KEY;
+
+export const APP_HOST: string = NODE_ENV === 'prod' ? prod.APP_HOST : dev.APP_HOST;
